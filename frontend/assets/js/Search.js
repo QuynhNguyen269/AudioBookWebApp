128ef16cda42bedaf18ba6f31d563e8a71c76970
QuynhNguyen269 <19520242@gm.uit.edu.vn>
2021-12-11 10:20:47 +0700

    complete audio book (adding search feature)

---
+++ frontend/assets/js/Search.js
@@ -0,0 +1,15 @@
+const btn =
+document.querySelector(".myBtn")
+const searchBox =
+document.querySelector(".fullScreen")
+const closeBtn = 
+document.querySelector(".closeBtn")
+  
+  
+closeBtn.addEventListener("click", ()=>{
+    searchBox.style.display = "none"
+})
+  
+btn.addEventListener('click', ()=>{
+    searchBox.style.display = "block"
+})
